22f396a21ef710d35940a9e8c3973355288ff38f
Ayuei <sefton_student@hotmail.com>
2017-05-12 17:47:41 +1000

    initial commit

---
+++ myClassifier.py
@@ -0,0 +1,28 @@
+import sys
+
+file_name, train_file, test_file, classifier = sys.argv
+
+class DecisionTree():
+	
+	#pseudo code
+	http://puu.sh/vNiO0/d05b24c98b.jpg
+
+	#Information gain definition
+	http://puu.sh/vNiLk/f82ecd39af.jpg
+
+
+def NaiveBayes():
+	#Probabiltiy Density Function
+	https://puu.sh/vNiSW/7caa2fde98.jpg
+
+	#Calculation of probabiltiies from training data
+	http://puu.sh/vNiVG/2438fbf0b3.jpg
+
+def main():
+	if classifier == 'nb':
+		#do
+	else:
+		#do
+
+if __name__ == '__main__'
+	sys.exit(main())

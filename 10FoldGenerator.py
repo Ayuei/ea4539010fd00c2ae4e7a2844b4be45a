e119ffe345720b52b60d5b61caf7422252d93a8e
Vincent Nguyen <vngu4919@uni.sydney.edu.au>
2017-05-17 14:18:56 +1000

    added more revisions

--- 10FoldGenerator.py
+++ 10FoldGenerator.py
@@ -2,7 +2,7 @@ import random
 from collections import defaultdict
 import sys
 in_file = open(sys.argv[1])
-out_file = open("pima-fold.pickle", "wb+")
+out_file = open("pima-fold.csv", "w+")
 data = in_file.read().splitlines()
 folds = defaultdict(list)
 
@@ -23,7 +23,13 @@ for i in range(len(yes_instances)):
 
 for i in range(len(no_instances)):
     folds['fold'+str(i%10+1)].append(no_instances[i])
-	
-import pickle
 
-pickle.dump(folds, out_file)
+for i in range(10):
+    out_file.write('fold'+str(i+1)+'\n')
+    for line in folds['fold'+str(i%10+1)]:
+	out_file.write(line+'\n')
+    out_file.write('\n')
+	
+#import pickle
+
+#pickle.dump(folds, out_file)
